ebacd436ca64a5dd27bcc6ca52de562004224b55
Navyashamli <navyasham412@gmail.com>
2024-03-11 04:17:02 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://png.pngitem.com/pimgs/s/175-1757251_blog-letters-word-font-internet-write-blogger-blog.png" alt="Welcome picture" />
+    </div>
+  )
+}
+
